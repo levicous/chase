59fb9d57c5efec9ddbb9f6a6df00ff749b14c690
levicous <159552581+levicous@users.noreply.github.com>
2021-04-16 20:29:11 +0100

    refactor: remove branding

---
+++ Chase/Chase/ChaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseApp.swift
+//  Chase
+//
+//  Created by Levicious on 2024-02-21.
+//
+
+import SwiftUI
+
+@main
+struct ChaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
